f5e29ffef193675cb17f99f21736d003346f7439
YikaiChek <chekyikai@gmail.com>
2013-11-17 01:05:09 +0800

    Create grades.java

---
+++ grades.java
@@ -0,0 +1,22 @@
+package hi;
+
+public class grades {
+ private String grades;
+
+ 
+
+
+public grades(String grades) {
+	this.grades = grades;
+	
+}
+
+public String getGrades(){
+	return grades;
+}
+
+
+public void setGrades(){
+	this.grades= grades;
+}
+}
