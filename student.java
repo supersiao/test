63e5d64f97bb806e51c13e10ec49fbfe2eff31b5
YikaiChek <chekyikai@gmail.com>
2013-11-16 22:17:32 +0800

    Create student.java

---
+++ student.java
@@ -0,0 +1,60 @@
+
+
+package hi;
+
+import java.util.ArrayList;
+
+public class student {
+
+	private String name;
+	private String matricNo;
+	private String gender;
+	private String courseName;
+	private String setCoursename;
+	
+public student(String name, String courseName, String matricNo){
+	
+	this.name = name;
+	this.matricNo = matricNo;
+	this.gender = gender;
+	this.courseName = courseName;
+	
+}
+	
+	public String getName(){
+		return name;
+		
+	}
+	
+	public String getMatricno(){
+		return matricNo;
+	}
+	
+	public String getGender(){
+		return gender;
+	}
+	
+	public String getCoursename(){
+		return courseName;
+	
+	}
+	
+	public void setName(){
+		this.name = name;
+	}
+	
+	public void setMatricno(){
+		this.matricNo = matricNo;
+		
+	}
+    
+	public void setGender(){
+		this.gender = gender;	
+	}
+	
+	public void setCoursename(){
+		this.setCoursename = courseName;
+	}
+}
+
+
