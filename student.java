7df75a3ee89a0e4b4acd58276e545323aad0a218
jasminehuuu <jasmine_hu6@msn.com>
2013-11-17 11:55:11 +0800

    Added new declaration and methods

--- student.java
+++ student.java
@@ -1,57 +1,59 @@
-package hi;
-
 import java.util.ArrayList;
 
 public class Student {
 
-	private String name;
-	private String matricNo;
-	private String gender;
-	private String courseName;
-	private String setCoursename;
-	
-public Student(String name, String matricNo, String gender, String courseName){
-	
-	this.name = name;
-	this.matricNo = matricNo;
-	this.gender = gender;
-	this.courseName = courseName;
-	
-}
-	
-	public String getName(){
-		return name;
-		
-	}
-	
-	public String getMatricno(){
-		return matricNo;
-	}
-	
-	public String getGender(){
-		return gender;
-	}
-	
-	public String getCoursename(){
-		return courseName;
-	
-	}
-	
-	public void setName(){
-		this.name = name;
-	}
-	
-	public void setMatricno(){
-		this.matricNo = matricNo;
-		
-	}
-    
-	public void setGender(){
-		this.gender = gender;	
-	}
-	
-	public void setCoursename(){
-		this.setCoursename = courseName;
-	}
+        private String name;
+        private String matricNo;
+        private String courseID;
+        private String attendance;
+        
+public Student(String name, String courseID, String matricNo, String attendance){
+        
+        this.name = name;
+        this.matricNo = matricNo;
+        this.courseID = courseID;
+        this.attendance = attendance;
+        
 }
 
+        public String getAttendance(){
+                return attendance;
+        }
+        
+        public String getName(){
+                return name;
+                
+        }
+        
+        public String getMatricno(){
+                return matricNo;
+        }
+        
+
+        public String getID(){
+                return courseID;
+        
+        }
+        
+        public void setName(String name){
+                this.name = name;
+        }
+        
+        public void setMatricno(String matricNo){
+                this.matricNo = matricNo;
+                
+        }
+    
+        public void setAttendance(String a){
+                this.attendance = a;
+        }
+        
+        public void setIDe(String c){
+                this.courseID = c;
+        }
+
+        public String toString() {
+        	   return this.name + "\t" + this.getID() + "\t" + this.getMatricno() +"\t" + this.getAttendance() ;
+        	   
+        	}
+}
