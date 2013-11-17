01caef6938c80b4ece0511c0bd1ec03180a6a178
jasminehuuu <jasmine_hu6@msn.com>
2013-11-17 11:56:00 +0800

    new Course Java file

---
+++ Course.java
@@ -0,0 +1,36 @@
+
+public class Course {
+	
+	private String courseID;
+	private String courseName;
+	
+	
+public Course( String courseID, String courseName){
+        
+        this.courseID = courseID;
+        this.courseName = courseName;    
+}
+
+public String getCourseID(){
+   return courseID;
+}
+
+
+public void setCourseID(String courseID){
+    this.courseID = courseID;
+}
+
+public String getCourseName(){
+    return courseName;
+}
+
+public void setCourseName(String cN){
+    this.courseName = cN;
+}
+
+public String toString() {
+	   return this.getCourseID() + "\t " + this.getCourseName() ;
+	   
+	}
+
+}
