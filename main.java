101c662bbc18fb175c681b48553b56c6e3380ab6
YikaiChek <chekyikai@gmail.com>
2013-11-17 15:35:16 +0800

    Version 1.1 implementation of user login

--- main.java
+++ main.java
@@ -1,28 +1,102 @@
-package hi;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
+import java.awt.TextField;
 import java.util.Scanner;
 
 public class main {
-	public static void main(String[] args) throws FileNotFoundException {
-		String inputFileName = args[0];
 
-		FileReader reader = new FileReader(inputFileName);
-		Scanner in = new Scanner(reader);
-		ArrayList<Student> al = new ArrayList<Student>();
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		String username;
+		String password;
 
-		while (in.hasNext()) {
-			String o = in.nextLine();
-			String[] name = o.split(",");
-			al.add(new Student(name[0], name[1], name[2], name[3]));
-		}
 
-		for (int i = 0; i < al.size(); i++) {
-			System.out.println("====");
-			System.out.println("Name: " + al.get(i).getName() + "\nMatriculation number: " + al.get(i).getMatricno() + "\nGender: " + al.get(i).getGender() + "\nCourse: " + al.get(i).getCoursename());
-			System.out.println("====");
-		}
+		Scanner scanner = new Scanner (System.in);
+		System.out.print("Enter your username");  
+		username = scanner.next(); 
+		Scanner scanner1 = new Scanner (System.in);
+		System.out.print("Enter your password");  
+		password = scanner1.next(); 
+		authenticate(username,password);
+
 	}
+
+
+	public static void authenticate(String username, String password) throws InterruptedException {
+
+		if (username.equals("admin") && password.equals("password")) {
+
+		
+		studentAttendance sa = new studentAttendance();
+		sa.initStudent();
+		sa.initCourse();
+		// tutorLoadGrade tg = new tutorLoadGrade();
+		int i = 1;
+		Scanner sc = new Scanner(System.in);
+
+		while (i != 0) {
+			System.out.println("Please enter an option?");
+			System.out.println("1.\t Grades");
+			System.out.println("2.\t Attendance");
+			System.out.println("3.\t Exit");
+
+			String ans = sc.nextLine();
+			Thread t = new Thread();
+			if (isInteger(ans)) {
+
+				switch (Integer.parseInt(ans)) {
+				case 1:
+
+					t.start();
+
+					for (i = 0; i < 4; i++) {
+						Thread.sleep(100);
+						System.out.print(".");
+					}
+
+					System.out.println("Database not found!");
+					System.out.println();
+
+					//tg.printUI();
+					break;
+
+				case 2:
+					sa.printUI();
+					break;
+
+				case 3:
+					i = 0;
+					System.out.println("Thank you!");
+					break;
+
+				default:
+					System.out.print("Wrong input, pls try again!");
+					System.out.println();
+
+				}
+
+			}
+
+		}
+		} 
+		else{
+			System.out.println("User and password not valid, Please try again!!");
+		}
+	
+	}
+
+
+
+
+	public static boolean isInteger(String str) {
+		int size = str.length();
+
+		for (int i = 0; i < size; i++) {
+			if (!Character.isDigit(str.charAt(i))) {
+				return false;
+			}
+		}
+
+		return size > 0;
+	}
+
 }
 
